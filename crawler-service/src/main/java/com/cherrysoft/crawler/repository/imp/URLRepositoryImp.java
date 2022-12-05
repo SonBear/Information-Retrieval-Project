da3464cf8b3e5e6e9c056250b05e9dc46f470fd9
HikingCarrot7 <ricardoibarra2044@gmail.com>
2022-12-04 20:27:57 -0600

    Added url repository layer & base models

---
+++ crawler-service/src/main/java/com/cherrysoft/crawler/repository/imp/URLRepositoryImp.java
@@ -0,0 +1,62 @@
+package com.cherrysoft.crawler.repository.imp;
+
+import com.cherrysoft.crawler.model.WebPageUrlSet;
+import com.cherrysoft.crawler.repository.URLRepository;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Repository;
+
+import java.io.*;
+import java.util.HashSet;
+import java.util.List;
+
+@Repository
+@Primary
+@RequiredArgsConstructor
+public class URLRepositoryImp implements URLRepository {
+  public static final String URLS_FILE_NAME = "urls.json";
+  private final Gson gson;
+
+  @Override
+  public void mergeAndSaveUrls(WebPageUrlSet urlList) {
+    WebPageUrlSet savedUrls = getSavedUrls();
+    WebPageUrlSet withNewUrls = savedUrls.mergedWith(urlList);
+    saveUrls(withNewUrls);
+  }
+
+  private void saveUrls(WebPageUrlSet urlSet) {
+    System.out.println(urlSet);
+    try (Writer writer = new FileWriter(URLS_FILE_NAME)) {
+      gson.toJson(urlSet.getUrls(), writer);
+    } catch (IOException e) {
+      e.printStackTrace();
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Override
+  public WebPageUrlSet getUnsavedUrls(WebPageUrlSet urlSet) {
+    WebPageUrlSet savedUrls = getSavedUrls();
+    return urlSet.asymmetricDiff(savedUrls);
+  }
+
+  @Override
+  public WebPageUrlSet getSavedUrls() {
+    try {
+      return tryGetSavedUrlSet();
+    } catch (IOException e) {
+      e.printStackTrace();
+      return WebPageUrlSet.EMPTY;
+    }
+  }
+
+  private WebPageUrlSet tryGetSavedUrlSet() throws IOException {
+    try (Reader reader = new FileReader(URLS_FILE_NAME)) {
+      List<String> savedUrls = gson.fromJson(reader, new TypeToken<>() {}.getType());
+      return new WebPageUrlSet(new HashSet<>(savedUrls));
+    }
+  }
+
+}
