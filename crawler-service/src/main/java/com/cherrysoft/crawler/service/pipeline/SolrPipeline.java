1b9194a4590434f38dfdd8bb9ed417475059b9ac
HikingCarrot7 <ricardoibarra2044@gmail.com>
2022-12-07 13:18:17 -0600

    Added WebPageUpdaterService to upload and delete documents from solr

--- crawler-service/src/main/java/com/cherrysoft/crawler/service/pipeline/SolrPipeline.java
+++ crawler-service/src/main/java/com/cherrysoft/crawler/service/pipeline/SolrPipeline.java
@@ -1,37 +1,22 @@
 package com.cherrysoft.crawler.service.pipeline;
 
+import com.cherrysoft.crawler.service.WebPageUpdaterService;
 import lombok.RequiredArgsConstructor;
-import org.apache.solr.client.solrj.SolrServerException;
-import org.apache.solr.client.solrj.impl.HttpSolrClient;
-import org.apache.solr.client.solrj.request.ContentStreamUpdateRequest;
-import org.apache.solr.common.util.ContentStreamBase;
-import org.apache.solr.common.util.NamedList;
-import org.springframework.http.MediaType;
 import us.codecraft.webmagic.ResultItems;
 import us.codecraft.webmagic.Task;
 import us.codecraft.webmagic.pipeline.Pipeline;
 
-import java.io.IOException;
-
 @RequiredArgsConstructor
 public class SolrPipeline implements Pipeline {
-  private final HttpSolrClient solrClient;
-  private final ContentStreamUpdateRequest req;
+  private final WebPageUpdaterService webPageUpdaterService;
 
   @Override
   public void process(ResultItems resultItems, Task task) {
     if (resultItems.getAll().isEmpty()) {
       return;
     }
-    String text = resultItems.get("text");
-    req.addContentStream(new ContentStreamBase.StringStream(text, MediaType.TEXT_HTML_VALUE));
-    try {
-      NamedList<Object> result = solrClient.request(req);
-      System.out.println("Result: " + result);
-    } catch (SolrServerException | IOException e) {
-      e.printStackTrace();
-      throw new RuntimeException(e);
-    }
+    String html = resultItems.get("text");
+    webPageUpdaterService.uploadWebPage(html);
   }
 
 }
