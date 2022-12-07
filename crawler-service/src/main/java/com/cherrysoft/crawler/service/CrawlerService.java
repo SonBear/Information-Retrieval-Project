7d5bdc9587a05009e079f3ab47821933bf6c3026
HikingCarrot7 <ricardoibarra2044@gmail.com>
2022-12-07 13:19:52 -0600

    Added functionality to reindex and delete urls

--- crawler-service/src/main/java/com/cherrysoft/crawler/service/CrawlerService.java
+++ crawler-service/src/main/java/com/cherrysoft/crawler/service/CrawlerService.java
@@ -1,52 +1,47 @@
 package com.cherrysoft.crawler.service;
 
-import com.cherrysoft.crawler.model.IndexedWebPagesResult;
 import com.cherrysoft.crawler.model.WebPageUrlSet;
-import com.cherrysoft.crawler.repository.URLRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import us.codecraft.webmagic.Spider;
-import us.codecraft.webmagic.monitor.SpiderMonitor;
 
 import javax.management.JMException;
 
 @Service
 @RequiredArgsConstructor
 public class CrawlerService {
-  private final Spider spider;
-  private boolean spiderRegistered = false;
-  private final URLRepository urlRepository;
+  private final SpiderFactory spiderFactory;
+  private Spider spider;
+  private WebPageUrlSet payload;
 
-  public IndexedWebPagesResult indexUrls(WebPageUrlSet urlSetPayload) {
-    WebPageUrlSet nonIndexedUrls = urlRepository.getUnsavedUrls(urlSetPayload);
-    crawlAndIndexUrls(nonIndexedUrls);
-    WebPageUrlSet failedToIndexUrlSet = urlRepository.getUnsavedUrls(urlSetPayload);
-
-    return IndexedWebPagesResult.builder()
-        .urlSetPayload(urlSetPayload)
-        .nonIndexedUrlSet(nonIndexedUrls)
-        .failedToIndexUrlSet(failedToIndexUrlSet)
-        .build();
-  }
-
-  private void crawlAndIndexUrls(WebPageUrlSet urlSetPayload) {
+  public void crawlAndIndexUrls(WebPageUrlSet payload) {
+    this.payload = payload;
+    this.spider = spiderFactory.createSpider();
     try {
-      tryCrawlAndIndexUrls(urlSetPayload);
+      tryCrawlAndIndexUrls();
     } catch (JMException e) {
       throw new RuntimeException(e);
     }
   }
 
-  private void tryCrawlAndIndexUrls(WebPageUrlSet urlSetPayload) throws JMException {
-    if (urlSetPayload.isEmpty()) {
+  private void tryCrawlAndIndexUrls() throws JMException {
+    if (emptyPayload()) {
       return;
     }
-    spider.addUrl(urlSetPayload.toArray());
-    if (!spiderRegistered) {
-      SpiderMonitor.instance().register(spider);
-      spiderRegistered = true;
-    }
+    addPayloadToSpider();
+    startSpider();
+  }
+
+  private void addPayloadToSpider() {
+    spider.addUrl(payload.toArray());
+  }
+
+  private void startSpider() {
     spider.run();
+  }
+
+  private boolean emptyPayload() {
+    return payload.isEmpty();
   }
 
 }
---
+++ crawler-service/src/main/java/com/cherrysoft/crawler/service/IndexingWebPagesService.java
@@ -0,0 +1,38 @@
+package com.cherrysoft.crawler.service;
+
+import com.cherrysoft.crawler.model.IndexedWebPagesResult;
+import com.cherrysoft.crawler.model.WebPageUrlSet;
+import com.cherrysoft.crawler.repository.URLRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class IndexingWebPagesService {
+  private final CrawlerService crawlerService;
+  private final WebPageUpdaterService webPageUpdaterService;
+  private final URLRepository urlRepository;
+
+  public IndexedWebPagesResult indexUrls(WebPageUrlSet payload) {
+    WebPageUrlSet nonIndexedUrls = urlRepository.getUnsavedUrls(payload);
+    crawlerService.crawlAndIndexUrls(nonIndexedUrls);
+    WebPageUrlSet failedToIndexUrlSet = urlRepository.getUnsavedUrls(payload);
+
+    return IndexedWebPagesResult.builder()
+        .urlSetPayload(payload)
+        .nonIndexedUrlSet(nonIndexedUrls)
+        .failedToIndexUrlSet(failedToIndexUrlSet)
+        .build();
+  }
+
+  public IndexedWebPagesResult reindexUrls(WebPageUrlSet urlSetToReindex) {
+    deleteUrls(urlSetToReindex);
+    return indexUrls(urlSetToReindex);
+  }
+
+  public WebPageUrlSet deleteUrls(WebPageUrlSet urlSetToDelete) {
+    webPageUpdaterService.deleteWebPages(urlSetToDelete);
+    return urlRepository.deleteUrls(urlSetToDelete);
+  }
+
+}
