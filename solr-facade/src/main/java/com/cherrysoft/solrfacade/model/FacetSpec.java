d794c356e5e3e2d0dee24fff32e830e0b468ba7f
HikingCarrot7 <ricardoibarra2044@gmail.com>
2022-12-09 22:38:21 -0600

    Added ParamSpec abstract class

--- solr-facade/src/main/java/com/cherrysoft/solrfacade/model/FacetSpec.java
+++ solr-facade/src/main/java/com/cherrysoft/solrfacade/model/FacetSpec.java
@@ -1,30 +1,12 @@
 package com.cherrysoft.solrfacade.model;
 
-import lombok.Data;
+import java.util.List;
+import java.util.Map;
 
-import java.util.Collection;
-import java.util.Set;
+public class FacetSpec extends ParamSpec {
 
-import static java.util.Objects.isNull;
-
-@Data
-public class FacetSpec {
-  private final Set<String> documentTypes;
-  private final Set<String> languages;
-
-  public String getDocumentTypesAsString() {
-    return joinCollectionBySpace(documentTypes);
-  }
-
-  public String getLanguagesAsString() {
-    return joinCollectionBySpace(languages);
-  }
-
-  private String joinCollectionBySpace(Collection<String> list) {
-    if (isNull(list)) {
-      return "";
-    }
-    return String.join(" ", list);
+  public FacetSpec(Map<String, List<String>> additionalParams) {
+    super(additionalParams);
   }
 
 }
--- solr-facade/src/main/java/com/cherrysoft/solrfacade/model/SearchSpec.java
+++ solr-facade/src/main/java/com/cherrysoft/solrfacade/model/SearchSpec.java
@@ -1,23 +1,28 @@
 package com.cherrysoft.solrfacade.model;
 
-import lombok.Data;
+import lombok.Getter;
+
+import java.util.List;
+import java.util.Map;
 
 import static com.cherrysoft.solrfacade.util.DictionaryConstants.AVAILABLE_DICTIONARIES;
 import static com.cherrysoft.solrfacade.util.DictionaryConstants.DICTIONARY_SPANISH;
-import static java.util.Objects.requireNonNullElse;
 
-@Data
-public class SearchSpec {
+@Getter
+public class SearchSpec extends ParamSpec {
   private final String query;
-  private final String dictionary;
 
-  public SearchSpec(String query, String dictionary) {
+  public SearchSpec(String query, Map<String, List<String>> additionalParams) {
+    super(additionalParams);
     this.query = query;
-    this.dictionary = requireNonNullElse(dictionary, DICTIONARY_SPANISH.getAlias());
   }
 
   public String getDictionary() {
-    return AVAILABLE_DICTIONARIES.get(dictionary);
+    List<String> dictionary = params.getOrDefault("dictionary", List.of(DICTIONARY_SPANISH.getAlias()));
+    if (!dictionary.isEmpty()) {
+      return AVAILABLE_DICTIONARIES.get(dictionary.get(0));
+    }
+    return DICTIONARY_SPANISH.getAlias();
   }
 
 }
