66b79e8674a1d5a267b5d667e18be2659d28f398
Emmanuel Chable <emmanuelisai_28@hotmail.com>
2022-12-12 14:49:22 -0600

    added suggestion items

--- frontend/src/components/search/documents/SearchPanel/SearchPanel.tsx
+++ frontend/src/components/search/documents/SearchPanel/SearchPanel.tsx
@@ -1,12 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import { appendQueryParams } from '../../../../utils/query-params';
-import { Button, Form, FormControlProps, InputGroup } from 'react-bootstrap';
+import {
+  Button,
+  Form,
+  FormControlProps,
+  InputGroup,
+  ListGroup,
+} from 'react-bootstrap';
 
 export const SearchPanel = () => {
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const [query, setQuery] = useState('');
+  const [suggesstions, setSuggestions] = useState(['soy una sugerencia']);
 
   useEffect(() => {
     const q = searchParams.get('query') || '';
@@ -28,23 +35,48 @@ export const SearchPanel = () => {
     }
   };
 
+  const suggestionStyle = {
+    width: '100%',
+    Position: 'absolute',
+    cursor: 'pointer',
+  };
+  const createSuugestions = (suggestions: any) => {
+    return (
+      <ListGroup style={suggestionStyle}>
+        {suggestions.map((a: any) => {
+          return (
+            <ListGroup.Item onClick={onClickSuggestion}>{a}</ListGroup.Item>
+          );
+        })}
+      </ListGroup>
+    );
+  };
+
+  const onClickSuggestion = (event: any) => {
+    setQuery(event.target.firstChild.data);
+    setSuggestions([]);
+  };
+
   const doSearch = () => {
     searchParams.set('query', query);
     navigate(appendQueryParams('/', searchParams.toString()));
   };
 
   return (
-    <InputGroup className="mb-3">
-      <Form.Control
-        placeholder="Potato AND Chips..."
-        aria-label="Potato AND Chips..."
-        value={query}
-        onChange={onQueryChanged}
-        onKeyDown={onKeyDown}
-      />
-      <Button variant="outline-secondary" onClick={onSearchClicked}>
-        Search
-      </Button>
-    </InputGroup>
+    <div>
+      <InputGroup className="mb-3">
+        <Form.Control
+          placeholder="Potato AND Chips..."
+          aria-label="Potato AND Chips..."
+          value={query}
+          onChange={onQueryChanged}
+          onKeyDown={onKeyDown}
+        />
+        {suggesstions.length > 0 && createSuugestions(suggesstions)}
+        <Button variant="outline-secondary" onClick={onSearchClicked}>
+          Search
+        </Button>
+      </InputGroup>
+    </div>
   );
 };
