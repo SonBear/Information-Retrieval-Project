c4aaa6623a69bb9760d9d2afb10788bb726a4430
HikingCarrot7 <ricardoibarra2044@gmail.com>
2022-12-11 23:34:27 -0600

    Changed from post to delete mapping for delete urls

--- crawler-service/src/main/java/com/cherrysoft/crawler/controller/IndexingWebPagesController.java
+++ crawler-service/src/main/java/com/cherrysoft/crawler/controller/IndexingWebPagesController.java
@@ -9,10 +9,7 @@ import com.cherrysoft.crawler.model.WebPageUrlSet;
 import com.cherrysoft.crawler.service.IndexingWebPagesService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -43,7 +40,7 @@ public class IndexingWebPagesController {
     return ResponseEntity.ok(urlSetMapper.toWebPageUrlSetDto(urlSet));
   }
 
-  @PostMapping("/urls")
+  @DeleteMapping("/urls")
   public ResponseEntity<WebPageUrlSetDTO> deleteUrls(@RequestBody @Valid WebPageUrlSetDTO urlSetDtoToDelete) {
     WebPageUrlSet urlSetToDelete = urlSetMapper.toWebPageUrlSet(urlSetDtoToDelete);
     WebPageUrlSet result = indexingWebPagesService.deleteUrls(urlSetToDelete);
